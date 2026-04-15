a99a7253e7e91eb217c0939a60e792e994cd585b
Shiri667377 <sw667377@gmail.com>
2026-04-16 00:00:50 +0300

    UI fix

--- client/src/screens/EnteringScreens/OnBoardingScreen/OnboardingScreen.tsx
+++ client/src/screens/EnteringScreens/OnBoardingScreen/OnboardingScreen.tsx
@@ -41,7 +41,7 @@ export const OnboardingScreen: React.FC = () => {
         id: "1",
         icon: "shield",
         title: "Monitor & Protect",
-        description: "Track screen time and block device instantly",
+        description: "Track screen time and block apps instantly",
       },
       {
         id: "2",
